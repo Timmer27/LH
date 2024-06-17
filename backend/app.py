c4b0772714004a72c019011e4b1ddd34cccca72f
Timmer <whdghtpgml@gmail.com>
2024-06-17 16:13:46 +0900

    모델 적용

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,15 @@
-from flask import Flask, jsonify, make_response
-# 내장 라이브러리
+from flask import Flask, jsonify, make_response, request
+from flask_cors import CORS
 import os, sys, time, warnings
 from datetime import datetime, timedelta
-
-# 외장 라이브러리
 import pandas as pd, numpy as np
-import dask.dataframe as dd
-from tqdm import tqdm
-from ckonlpy.tag import Twitter
-
-# 딥러닝 라이브러리
-from sentence_transformers import SentenceTransformer, models, datasets, losses, util
-from torch.utils.data import DataLoader
 import torch
-import json
-import re
-from sentence_transformers import util
-from sklearn.metrics import jaccard_score
-from ckonlpy.tag import Twitter
+from detect import detect
 # 경고 무시
 warnings.filterwarnings('ignore')
 
-# 사용자 모듈
-from common import *
-from func_collection import *
-
 app = Flask(__name__)
+CORS(app)
 
 # 날짜 설정
 today = '20240415'
@@ -36,10 +20,49 @@ src_path = '/home/tako/eoks/lh/lh_dev2/src'
 # GPU 설정
 # torch.cuda.set_device(1)
 
+UPLOAD_FOLDER = 'source'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 # 경로 추가
 if src_path not in sys.path:
     sys.path.append(src_path)
 
+def tensor_to_serializable(data):
+    """Converts nested tensors in a structure to serializable formats."""
+    if isinstance(data, torch.Tensor):
+        return data.tolist()  # convert tensor to list
+    elif isinstance(data, (list, tuple)):
+        return [tensor_to_serializable(item) for item in data]
+    elif isinstance(data, dict):
+        return {key: tensor_to_serializable(value) for key, value in data.items()}
+    return data
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    if file:
+        filename = file.filename
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(file_path)
+        return jsonify({'file_name': filename, 'file_path': file_path}), 200
+
+
+@app.route('/predict', methods=['POST'])
+def pred_img():
+    file_name = request.form.get('file_name')
+    file_path = request.form.get('file_path')
+    lines = detect(saved_img_name=file_name, saved_txt_name=file_name+'.txt', source=file_path, device="cpu") #device=0
+    # Convert tensors to serializable formats
+    serializable_lines = tensor_to_serializable(lines)
+    
+    # Convert to JSON response
+    return jsonify(serializable_lines)
 
 @app.route('/', methods=['GET'])
 def test_response():
@@ -61,3 +84,6 @@ def test_response():
     # https://developer.mozilla.org/en-US/docs/Web/HTTP/Server-Side_Access_Control
 
     return response
+
+if __name__ == "__main__":
+    app.run()
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const BACKEND_URL = 'http://localhost:80/api'
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+  const [labels, setLabels] = useState("");
+
   const [text, setText] = useState("");
   const [topK, setTopK] = useState(3);
   const [workType, setWorkType] = useState("");
-  // Define state for other input fields similarly
+
+  const [loading, setLoading] = useState(false); // State to manage loading spinner
 
   const handleButtonClick = () => {
     // Handle button click event here
@@ -12,31 +19,137 @@ function App() {
     // Update state or display output accordingly
   };
 
+  const handleUpload = async () => {
+    setLoading(true);
+    if (!selectedFile) {
+      alert("Please select an image first.");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    try {
+      console.log('process.env.REACT_BACKEND_URLprocess.env.REACT_BACKEND_URLprocess.env.REACT_BACKEND_URL', BACKEND_URL)
+      const response = await axios.post(
+        `${BACKEND_URL}/upload`,
+        // "http://localhost:5000/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      return response.data;
+      // console.log();
+    } catch (error) {
+      console.error("There was an error uploading the image!", error);
+    }
+  };
+
+  const handlePredict = async (file_name, file_path) => {
+    const formData = new FormData();
+    formData.append("file_name", file_name);
+    formData.append("file_path", file_path);
+
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/predict`,
+        // "http://localhost:5000/predict",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setLabels(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("There was an error uploading the image!", error);
+    }
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file && file.type === "image/jpeg") {
+      setSelectedFile(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert("Please select a JPEG image.");
+      setPreview(null);
+      return false;
+    }
+  };
+
   return (
-    <div className="flex flex-col p-12">
-      <h1 className="text-3xl font-bold mb-8">텍스트 모델 데모 시연</h1>
-      <label className="mb-2">스케줄링 모델을 사용합니다.</label>
-      <textarea
-        className="border mb-6 p-2"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="입력해주세요"
-      />
-      {/* Define other input fields */}
-      <label className="mb-2">출력 값 개수</label>
-      <input
-        className="border mb-4 p-2"
-        type="number"
-        value={topK}
-        onChange={(e) => setTopK(parseInt(e.target.value))}
-      />
-      <button
-        onClick={handleButtonClick}
-        className="border p-2 bg-slate-600 text-white rounded-md"
-      >
-        제출하기
-      </button>
-      {/* Display output */}
+    <div className="flex">
+      <div className="flex flex-col p-12 flex-1">
+        <h1 className="text-3xl font-bold mb-8">이미지 모델 데모 시연</h1>
+        <label className="mb-2">detect.py 실행</label>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          className="mb-4 p-2 border rounded"
+        />
+        {preview && (
+          <div className="mt-4">
+            <img
+              src={preview}
+              alt="Preview"
+              className="w-64 h-64 object-contain border rounded"
+            />
+          </div>
+        )}
+        <button
+          disabled={!selectedFile}
+          onClick={async () => {
+            const data = await handleUpload();
+            await handlePredict(data.file_name, data.file_path);
+          }}
+          className="border p-2 bg-slate-600 text-white rounded-md"
+        >
+          제출하기
+        </button>
+        {loading && (
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div className="border-t-4 border-b-4 border-blue-500 rounded-full w-12 h-12 animate-spin"></div>
+          </div>
+        )}
+        {labels && <div>result labels: {labels}</div>}
+      </div>
+      <div className="flex flex-col p-12 flex-1">
+        <h1 className="text-3xl font-bold mb-8">
+          텍스트 모델 데모 시연 (미완료)
+        </h1>
+        <label className="mb-2">스케줄링 모델을 사용합니다.</label>
+        <textarea
+          className="border mb-6 p-2"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="입력해주세요"
+        />
+        {/* Define other input fields */}
+        <label className="mb-2">출력 값 개수</label>
+        <input
+          className="border mb-4 p-2"
+          type="number"
+          value={topK}
+          onChange={(e) => setTopK(parseInt(e.target.value))}
+        />
+        <button
+          onClick={handleButtonClick}
+          className="border p-2 bg-slate-600 text-white rounded-md"
+        >
+          제출하기
+        </button>
+        {/* Display output */}
+      </div>
     </div>
   );
 }
